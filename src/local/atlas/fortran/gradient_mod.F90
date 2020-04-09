bc383683a1894234ad8a242d897957cefd8d2064
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2020-04-09 15:48:16 +0000

    Gradient (order=1) works with N16 X/Y/Z wrt lon/lat

--- src/local/atlas/fortran/gradient_mod.F90
+++ src/local/atlas/fortran/gradient_mod.F90
@@ -16,19 +16,21 @@ public :: gradient
 private
 
 interface
-function gradient__ (pgp) bind (C, name="gradient__")
+function gradient__ (fs, pgp) bind (C, name="gradient__")
     use iso_c_binding, only: c_ptr
     type (c_ptr) :: gradient__
+    type (c_ptr), value :: fs
     type (c_ptr), value :: pgp
 end function
 end interface
 
 contains
 
-function gradient (pgp) result (pgpg)
+function gradient (fs, pgp) result (pgpg)
+  type (atlas_functionspace_StructuredColumns), intent(in) :: fs
   type (atlas_FieldSet), intent(in) :: pgp  
   type (atlas_FieldSet) :: pgpg
-  pgpg = atlas_FieldSet (gradient__ (pgp%CPTR_PGIBUG_A)) 
+  pgpg = atlas_FieldSet (gradient__ (fs%CPTR_PGIBUG_A, pgp%CPTR_PGIBUG_A)) 
   call pgpg%detach () ! Required here, because this comes from a temporary atlas::FieldSet object
                       ! and the implementation object had its count increased to
                       ! avoid deletion
--- src/local/atlas/gradient/gradient.cc
+++ src/local/atlas/gradient/gradient.cc
@@ -1,9 +1,12 @@
+#include "gradient.h"
+
 #include "atlas/util/Config.h"
 #include "atlas/grid.h"
 #include "atlas/array.h"
 #include "atlas/parallel/mpi/mpi.h"
 #include "atlas/functionspace.h"
 
+#include <limits>
 #include <stdio.h>
 #include <math.h>
 
@@ -36,10 +39,17 @@ gradient (const atlas::functionspace::StructuredColumns & fs, const atlas::Field
   int irank = comm.rank (), nproc = comm.size ();
 
   atlas::FieldSet pgpg;
+  atlas::Field fxy = fs.xy ();
 
   const atlas::StructuredGrid & grid = fs.grid ();
 
+
+
   fs.haloExchange (pgp);
+  fs.haloExchange (fxy);
+
+  printf (" pgp.size () = %8d\n", pgp.size ());
+
 
   for (int jfld = 0; jfld < pgp.size (); jfld++)
     {
@@ -68,6 +78,8 @@ gradient (const atlas::functionspace::StructuredColumns & fs, const atlas::Field
 
     }
 
+      printf (" fs.size (), fs.sizeOwned () = %8d, %8d\n", fs.size (), fs.sizeOwned ());
+
   auto iv = atlas::array::make_view<int,1> (fs.index_i ());
   auto jv = atlas::array::make_view<int,1> (fs.index_j ());
 
@@ -75,81 +87,132 @@ gradient (const atlas::functionspace::StructuredColumns & fs, const atlas::Field
 
   const auto & xspc = grid.xspace ();
 
+  printf (" glob = %d\n", glob);
   printf (" grid.ny () = %8d\n", grid.ny ());
+
+  for (int j = 0; j < grid.ny (); j++)
+    printf (" %8d < %8d\n", j, grid.nx (j));
+
+  printf ("\n");
+
+  printf ("i_begin, i_end\n");
+  for (int j = fs.j_begin (); j < fs.j_end (); j++)
+    printf ("%8d > %8d, %8d\n", j, fs.i_begin (j), fs.i_end (j));
+
+  printf ("\n");
+
+  printf ("i_begin_halo, i_end_halo\n");
+  for (int j = fs.j_begin_halo (); j < fs.j_end_halo (); j++)
+    printf ("%8d > %8d, %8d\n", j, fs.i_begin_halo (j), fs.i_end_halo (j));
+
+  printf ("\n");
+
+  printf (" %8s | %8s, %8s | %8s, %8s | %8s, %8s | %8s, %8s | %8s, %8s | %8s, %8s | %8s, %8s | %8s, %8s |\n", 
+              "jloc", "i", "j", "inw", "jnw", "ine", "jne", "isw", "jsw", "ise", "jse", "i_w", "j_w", "i_e", "j_e", "jlocw", "jloce");
+
+  int inv = std::numeric_limits<int>::max ();
+
+  auto vxy = atlas::array::make_view<double,2> (fxy);
+
+  for (int j = fs.j_begin_halo (); j < fs.j_end_halo (); j++)
+  for (int i = fs.i_begin_halo (j); i < fs.i_end_halo (j); i++)
+    {
+      atlas::idx_t idx = fs.index (i, j);
+      printf (" %8d, %8d > %12.4f, %12.4f\n", i, j, vxy (idx, 0), vxy (idx, 1));
+    }
 
   for (int jloc = 0; jloc < fs.sizeOwned (); jloc++)
     {
+
+bool dbg = (irank == 0) && (jloc == 0);
+
       int i = iv (jloc) - 1, j = jv (jloc) - 1;
 
-      atlas::PointXY xy = grid.xy (i, j);
-
-      if ((j == 0) || (j == grid.ny () - 1))
-        continue;
+      atlas::PointXY xy = atlas::PointXY (vxy (jloc, 0), vxy (jloc, 1));
 
       int jm = j - 1, j0 = j + 0, jp = j + 1;
+      int im = i - 1, i0 = i + 0, ip = i + 1;
 
       auto xj_to_imip = [&] (double x, int j, int & im, int & ip)
       {
-        
-        int inx = xspc.nx ()[j];
-        double dx = xspc.dx ()[j];
-        double xmin = xspc.xmin ()[j];
+        int js = glob ? std::min (grid.ny () - 1, std::max (0, j)) : j; // Pole
+        double dx = xspc.dx ()[js];
+        double xmin = xspc.xmin ()[js];
         im = floor ((x - xmin) / dx); 
         ip = im + 1;
-        if (! glob)
-          {
-            if ((im < 0) || (im >= inx)) im = -1;
-            if ((ip < 0) || (ip >= inx)) ip = -1;
-          }
       };
 
       int inw, jnw = jm, ine, jne = jm,  
-          isw, jsw = jp, ise, jse = jp;
+          isw, jsw = jp, ise, jse = jp,
+          i_w, j_w = j0, i_e, j_e = j0;
 
       xj_to_imip (xy.x (), jnw, inw, ine);
       xj_to_imip (xy.x (), jsw, isw, ise);
 
-      auto check_ij = [&] (int i, int j)
+if (dbg)
+printf (" inw, jnw = %8d, %8d\n", inw, jnw);
+
+      i_w = i - 1;
+      i_e = i + 1;
+
+      auto ij_to_index_and_xy = [&] (int i, int j)
       {
-        if ((i < 0) || (j < 0))
-          return;
-        if (j <  fs.j_begin_halo ( )) { printf ("%s:%d\n", __FILE__, __LINE__); abort (); }
-        if (j >= fs.j_end_halo   ( )) { printf ("%s:%d\n", __FILE__, __LINE__); abort (); }
-        if (i <  fs.i_begin_halo (j)) { printf ("%s:%d\n", __FILE__, __LINE__); abort (); }
-        if (i >= fs.i_end_halo   (j)) { printf ("%s:%d\n", __FILE__, __LINE__); abort (); }
+        if ((j < fs.j_begin_halo ()) || (fs.j_end_halo () <= j))
+          return inv;
+        if ((i < fs.i_begin_halo (j)) || (fs.i_end_halo (j) <= i))
+          return inv;
+        atlas::idx_t idx = fs.index (i, j);
+        return idx;
       };
-
-      auto ij_to_index_and_xy = [&] (bool w, bool e, int i, int j, atlas::PointXY & xy)
-      {
-        if ((i < 0) || (j < 0))
-          return -1;
-        xy = grid.StructuredGrid::xy (i, j);
-
-        if (glob)
-          {
-            if (w && (i == xspc.nx ()[j]-1)) 
-              xy = atlas::PointXY (-xspc.dx ()[j], xy.y ());
-            if (e && (i == 0))  // TODO: check xmax
-              xy = atlas::PointXY (xspc.xmax ()[j] + xspc.dx ()[j], xy.y ());
-          }
-
-        return fs.index (i, j);
-      };
-
-      atlas::PointXY xynw, xyne, xysw, xyse;
 
       atlas::idx_t 
-            jlocnw = ij_to_index_and_xy (true, false, jnw, inw, xynw), 
-            jlocne = ij_to_index_and_xy (false, true, jne, ine, xyne),
-            jlocsw = ij_to_index_and_xy (true, false, jsw, isw, xysw), 
-            jlocse = ij_to_index_and_xy (false, true, jse, ise, xyse);
+           jlocnw = ij_to_index_and_xy (inw, jnw), 
+           jlocne = ij_to_index_and_xy (ine, jne),
+           jlocsw = ij_to_index_and_xy (isw, jsw), 
+           jlocse = ij_to_index_and_xy (ise, jse),
+           jloc_w = ij_to_index_and_xy (i_w, j_w),
+           jloc_e = ij_to_index_and_xy (i_e, j_e);
 
+if (0)
+if (j == 0)
+{
+  printf (" jloc = %8d\n", jloc);
 
-      printf ("| %8d, %8d | %8d, %8d | %8d, %8d | %8d, %8d | %8d, %8d |\n", 
-              i, j, inw, jnw, ine, jne, isw, jsw, ise, jse);
+  printf (" jlocnw, jlocne, jlocsw, jlocse, jloc_w, jloc_e = %8d, %8d, %8d, %8d, %8d, %8d\n", 
+            jlocnw, jlocne, jlocsw, jlocse, jloc_w, jloc_e);
 
+  continue;
 
-  
+}
+
+      for (int jfld = 0; jfld < pgp.size (); jfld++)
+        {
+          auto v  = atlas::array::make_view<double,1> (pgp [  jfld  ]);
+          auto vx = atlas::array::make_view<double,1> (pgpg[2*jfld+0]);
+          auto vy = atlas::array::make_view<double,1> (pgpg[2*jfld+1]);
+
+          double vw = v (jloc_w);
+          double ve = v (jloc_e);
+          double xw = vxy (jloc_w, 0);
+          double xe = vxy (jloc_e, 0);
+
+          vx (jloc) = (vw - ve) / (xw - xe);
+
+          double an = (vxy (jloc, 0) - vxy (jlocne, 0)) / (vxy (jlocnw, 0) - vxy (jlocne, 0));
+          double as = (vxy (jloc, 0) - vxy (jlocse, 0)) / (vxy (jlocsw, 0) - vxy (jlocse, 0));
+   
+          double vn = an * v (jlocnw) + (1.0 - an) * v (jlocne);
+          double vs = as * v (jlocsw) + (1.0 - as) * v (jlocse);
+          double yn = vxy (jlocnw, 1);
+          double ys = vxy (jlocsw, 1);
+
+          if (glob && (j == 0))
+            yn = +180.0 - yn;
+          if (glob && (j == grid.ny () - 1))
+            ys = -180.0 + ys;
+
+          vy (jloc) = (vn - vs) / (yn - ys);
+        }
 
 
     }

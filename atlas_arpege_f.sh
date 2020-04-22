ddd1bbaad24839a7f679e784a120ddd6eaa7c2ba
Florian Suzat <florian.suzat@meteo.fr>
2020-04-22 09:01:24 +0000

    halffdiff v0

--- atlas_arpege_f.sh
+++ atlas_arpege_f.sh
@@ -23,30 +23,30 @@ rm -rf XYZ1* stdeo.* XYZ2I*
 
 PACK=/home/gmap/mrpm/marguina/pack/46t1_distatlas.01.I185274INTELMPI184274MT.x
 
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 1 -- $PACK/bin/ATLAS_ARPEGE_F --grid2 fort.4.t32
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid2 t31/ICMSHARPEINIT
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid2 fort.4.64x64  --dist2 checkerboard
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid2 ICMSHAROMINIT --dist2 checkerboard
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid2 ICMSHAROMINIT --dist2 checkerboard
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid2 fort.4.t32 --dist2 equal_regions
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 S80x40 --dist1 checkerboard --grid2 fort.4.t32 --dist2 equal_regions
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 fort.4.t32 --dist1 equal_regions --grid2 fort.4.64x64 --dist2 checkerboard
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 fort.4.64x64 --dist1 checkerboard --grid2 fort.4.32x32 --dist2 checkerboard --write1
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 L80x40 --dist1 checkerboard --grid2 fort.4.32x32 --dist2 checkerboard 
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 fort.4.3x3 --dist1 checkerboard --grid2 fort.4.32x32 --dist2 checkerboard
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 1 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 fort.4.t32 --dist1 equal_regions --grid2 fort.4.32x32 --dist2 checkerboard
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 X80x40 --dist1 checkerboard --grid2 fort.4.t32 --dist2 equal_regions
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 1 -- $PACK/bin/ATLAS_ARPEGE_F --grid2 fort.4.t32
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid2 t31/ICMSHARPEINIT
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid2 fort.4.64x64  --dist2 checkerboard
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid2 ICMSHAROMINIT --dist2 checkerboard
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid2 ICMSHAROMINIT --dist2 checkerboard
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid2 fort.4.t32 --dist2 equal_regions
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 S80x40 --dist1 checkerboard --grid2 fort.4.t32 --dist2 equal_regions
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 fort.4.t32 --dist1 equal_regions --grid2 fort.4.64x64 --dist2 checkerboard
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 fort.4.64x64 --dist1 checkerboard --grid2 fort.4.32x32 --dist2 checkerboard --write1
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 L80x40 --dist1 checkerboard --grid2 fort.4.32x32 --dist2 checkerboard 
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 fort.4.3x3 --dist1 checkerboard --grid2 fort.4.32x32 --dist2 checkerboard
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 1 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 fort.4.t32 --dist1 equal_regions --grid2 fort.4.32x32 --dist2 checkerboard
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 X80x40 --dist1 checkerboard --grid2 fort.4.t32 --dist2 equal_regions
 
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 L160x80 --dist1 checkerboard --grid2 fort.4.t32 --dist2 equal_regions --interpA
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 L40x20 --dist1 checkerboard --grid2 fort.4.t32 --dist2 equal_regions --interpA
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 L160x80 --dist1 checkerboard --grid2 fort.4.t32 --dist2 equal_regions --interpA
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 L40x20 --dist1 checkerboard --grid2 fort.4.t32 --dist2 equal_regions --interpA
 
 
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 2 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 L40x20 --dist1 checkerboard --grid2 fort.4.64x64 --dist2 checkerboard --interpA
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 N320 --dist1 equal_regions --grid2 fort.4.64x64 --dist2 checkerboard --interpA --write1
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 fort.4.64x64 --dist1 checkerboard --grid2 fort.4.32x32_100km --dist2 checkerboard --interpA --write1
-#~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 1 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 fort.4.64x64 --dist1 checkerboard --grid2 fort.4.32x32_100km --dist2 checkerboard --interpA --write1
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 2 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 L40x20 --dist1 checkerboard --grid2 fort.4.64x64 --dist2 checkerboard --interpA
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 N320 --dist1 equal_regions --grid2 fort.4.64x64 --dist2 checkerboard --interpA --write1
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 fort.4.64x64 --dist1 checkerboard --grid2 fort.4.32x32_100km --dist2 checkerboard --interpA --write1
+#~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 1 -- $PACK/bin/ATLAS_ARPEGE_F --grid1 fort.4.64x64 --dist1 checkerboard --grid2 fort.4.32x32_100km --dist2 checkerboard --interpA --write1
 
- ~/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F \
+ ~marguina/SAVE/mpiauto/mpiauto --wrap --wrap-stdeo -np 4 -- $PACK/bin/ATLAS_ARPEGE_F \
      --grid1 L400x200 --grid2 PFFCSTEUROC25+0000 --interp4 --write2
 
 if [ 0 -eq 1 ]
@@ -56,7 +56,7 @@ export ATLAS_TRACE=1
 export ATLAS_TRACE_REPORT=1
 
 # --prefix-command '/home/gmap/mrpm/marguina/bin/perf_wrap --call-graph fp' \
-~/SAVE/mpiauto/mpiauto \
+~marguina/SAVE/mpiauto/mpiauto \
   --prefix-mpirun '/usr/bin/time -f "time=%es"' \
   --prefix-command '/usr/bin/time -f "mem=%Mkb"' \
   --wrap --wrap-stdeo -nn $NN -nnp 8 -openmp 16 -- $PACK/bin/ATLAS_ARPEGE_F \
@@ -70,7 +70,7 @@ export ATLAS_TRACE=1
 export ATLAS_TRACE_REPORT=1
 
 # --prefix-command '/usr/bin/time -f "mem=%Mkb"' \
-~/SAVE/mpiauto/mpiauto \
+~marguina/SAVE/mpiauto/mpiauto \
   --prefix-mpirun '/usr/bin/time -f "time=%es"' \
   --prefix-command '/home/gmap/mrpm/marguina/bin/perf_wrap --call-graph fp' \
   --wrap --wrap-stdeo -nn $NN -nnp 4 -openmp 32 -- $PACK/bin/ATLAS_ARPEGE_F \
@@ -80,9 +80,9 @@ export ATLAS_TRACE_REPORT=1
 elif [ 0 -eq 1 ]
 then
 
-~/SAVE/mpiauto/mpiauto \
+~marguina/SAVE/mpiauto/mpiauto \
   --prefix-mpirun '/usr/bin/time -f "time=%es"' \
-  --prefix-command '/usr/bin/time -f "mem=%Mkb"' \
+/bin/bash: :49: command not found
   --wrap --wrap-stdeo -nn $NN -nnp 4 -openmp 32 -- $PACK/bin/ATLAS_ARPEGE_F \
   --grid1 L4000x2000 --dist1 checkerboard  --block1 4000 --light1 \
   --grid2 N512       --dist2 equal_regions --interpA 
@@ -90,7 +90,7 @@ then
 elif [ 0 -eq 1 ]
 then
 
-~/SAVE/mpiauto/mpiauto \
+~marguina/SAVE/mpiauto/mpiauto \
   --prefix-mpirun '/usr/bin/time -f "time=%es"' \
   --prefix-command '/usr/bin/time -f "mem=%Mkb"' \
   --wrap --wrap-stdeo -nn $NN -nnp 1 -openmp 1 -- $PACK/bin/ATLAS_ARPEGE_F \
--- src/local/trans/module/atlas-arpege_module.F90
+++ src/local/trans/module/atlas-arpege_module.F90
@@ -463,7 +463,7 @@ BLOCK
   YLFLDL (JFLD) = YDFLSL%FIELD (JFLD)
 
   CLNOMA = YLFLDL (JFLD)%NAME ()
-
+  WRITE(*,*) 'WRITING ',CLNOMA
 ! Global field; zero size if this proc does not write the field
   ISIZEG = 0
   IF (ITO (JFLD) == MYPROC) ISIZEG = INT (YDGRID%SIZE (), JPIM)
